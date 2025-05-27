ed6f893e0f1c0f8ae98ba0cff0508e764e1fc60d
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-27 16:49:05 -0300

    Update preload.cjs

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,1 +1,6 @@
 const { contextBridge, ipcRenderer} = require('electron')
+
+contextBridge.exposeInMainWorld('electronAuth', {
+  login: (data) => ipcRenderer.invoke('auth:login', data),
+  register: (data) => ipcRenderer.invoke('auth:register', data),
+})
