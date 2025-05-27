8e127afa904ebae25ceedfe923778df0639a552e
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-27 20:00:12 -0300

    Update UserFacade.cjs

--- electron/database/facades/UserFacade.cjs
+++ electron/database/facades/UserFacade.cjs
@@ -1,7 +1,17 @@
-class UserFacade {
-  static async getUserByEmail(){
+const db = require('../knex.cjs')
 
+class UserFacade {
+  static async getUserByEmail(email){
+    try {
+      const response = await db('users').where('email', email).first()
+      if(!response){
+        return {error: true, message: 'User not found!'}
+      }
+      return {error: false, user: response}
+    } catch (error) {
+      return {error: true}
+    }
   }
 }
-
+UserFacade.getUserByEmail('Ayana.Tromp61@yahoo.com').then(res => console.log(res))
 module.exports = UserFacade
