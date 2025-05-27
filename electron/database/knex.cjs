2c6446567f3e5faa7caa5010182e5710cddb94de
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-27 20:00:14 -0300

    Update knex.cjs

--- electron/database/knex.cjs
+++ electron/database/knex.cjs
@@ -1,5 +1,6 @@
 const knex = require('knex')
 const knexfile = require('./knexfile.cjs')
+const path = require('node:path')
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const environment = process.env.NODE_ENV || 'development'
