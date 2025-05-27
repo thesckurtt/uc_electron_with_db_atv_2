2b1b2785a7f9ee2b26408646f0eddc3c1291a6ff
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-27 17:44:18 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import ProtectedRoute from './components/ProtectedRoute'
 import Dashboard from './pages/Dashboard'
 import Register from './pages/Register'
-import Home from './pages/Home'
 
 const App = () => {
   return (
     <Router basename='/'>
       <Routes>
-        <Route path='/loginm' element={<Home />} />
-        <Route path='/' element={<Login />} />
+        <Route path='/' element={<Navigate to={'/login'} replace />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/dashboard' element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path='*' element={<h1>Index</h1>} />
