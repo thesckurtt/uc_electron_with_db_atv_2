8ae8115b9231240dbf5203d6d6f7ae507aee72fe
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-27 19:42:06 -0300

    Create user_seed.cjs

---
+++ electron/database/seeds/user_seed.cjs
@@ -0,0 +1,20 @@
+const { faker } = require('@faker-js/faker');
+const bcryptjs = require('bcryptjs')
+exports.seed = async function (knex) {
+  // Limpa a tabela antes de inserir dados
+  await knex('users').del();
+
+  const users = [];
+
+  for (let i = 0; i < 10; i++) {
+    const hashedPassword = await bcryptjs.hash(faker.internet.password(10), 10); // 10 rounds de salt
+
+    users.push({
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: hashedPassword // senha aleatória (texto simples)
+    });
+  }
+
+  await knex('users').insert(users);
+};
