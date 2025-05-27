96fd4ee8f6d59b0dd7cf16324f383bb61a8971ed
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-27 17:15:25 -0300

    Update InptAuthForm.jsx

--- src/components/InptAuthForm.jsx
+++ src/components/InptAuthForm.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const InptAuthForm = ({ name, type, text = null, placeholder }) => {
+const InptAuthForm = ({ name, type, text = null, placeholder, value, setValue }) => {
   return (
     <div class="mb-3">
-      <label for={name} class="form-label">{text}</label>
-      <input type={type ?? 'text'} class="form-control" id={name} placeholder={placeholder} />
+      <label for={name} class="form-label fw-medium">{text}</label>
+      <input type={type ?? 'text'} class="form-control" value={value} onChange={(e) => setValue(e.target.value)} id={name} placeholder={placeholder} />
     </div>
   )
 }
